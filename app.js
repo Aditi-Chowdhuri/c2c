4427cffe11c43ae851dbe649c4c615ec6c2168b5
Aditi-Chowdhuri <46585556+Aditi-Chowdhuri@users.noreply.github.com>
2020-03-08 15:57:57 +0530

    pages incorporated

--- app.js
+++ app.js
@@ -2,11 +2,24 @@
 var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
+    //var firebaseConfig = {
+    //apiKey: 'AIzaSyCxrQzDNrYKkB-WqinKQ68fDhtQbStCt5A',
+    //authDomain: '<your-auth-domain>',
+    //databaseURL: '<your-database-url>',
+    //storageBucket: '<your-storage-bucket-url>'
+  //};
+  //firebase.initializeApp(firebaseConfig);
+
+  // Get a reference to the storage service, which is used to create references in your storage bucket
+  //var storage = firebase.storage(); 
+
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger");
+
+    // AIzaSyCxrQzDNrYKkB-WqinKQ68fDhtQbStCt5A
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -26,10 +39,25 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.src = cameraSensor.toDataURL();
     cameraOutput.classList.add("taken");
+    console.log(cameraOutput.src);
+    // Canvas2Image.saveAsPNG(cameraOutput.src);
+    var aLink = document.createElement('a');
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("click");
+    aLink.download = 'image.png';
+    aLink.href = cameraOutput.src;
+    var download = document.createElement('a');
+    download.href = image.png;
+    download.download = 'image.png';
+    download.click();
+    console.log(download.click);
+    aLink.dispatchEvent(evt);
     // track.stop();
 };
+
+
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
